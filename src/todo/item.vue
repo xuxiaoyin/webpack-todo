5b8fd16f4f3a83bf1ebaf0628677b5b19d84e0ee
xuxiaoyin <279238850@qq.com>
2019-04-25 00:03:50 +0800

    配置

---
+++ src/todo/item.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="['todo-item', todo.completed ? 'completed' : '']">
+    <input 
+      type="checkbox"
+      class="toggle"
+      v-model="todo.completed"
+    >
+    <label>{{todo.content}}</label>
+    <button class="destory" @click="deleteTodo"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    }
+  },
+  methods: {
+    deleteTodo() {}
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.todo-item{
+  position: relative;
+  background: #fff;
+  font-size: 24px;
+  border-bottom: 1px solid rgba(0,0,0,0.06)
+  &:hover{
+    .destory:after{
+      content: 'x'
+    }
+  }
+  lable{
+    white-space: pre-line;
+    word-break: break-all;
+    padding: 15px 60px 15px 15px;
+    margin-left: 45px;
+    display: block;
+    line-height: 1.2;
+  }
+}
+</style>
+
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 通过 npm 安装
+const webpack = require('webpack'); // 用于访问内置插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname,'dist')
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HtmlWebpackPlugin(),
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.gif|jpg|jpeg|png|svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name:'[name]-aaa.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true
+    },
+    hot: true
+    // historyFallback: {
+
+    // },
+    // open: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+}
+
+module.exports = config
